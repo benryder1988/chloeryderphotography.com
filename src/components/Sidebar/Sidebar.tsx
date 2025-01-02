0d1feccbd0f89f96c5f710eba9e1768b20447112
benryder1988 <benryder1988.br@gmail.com>
2025-01-02 22:11:33 +0100

    test

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,14 @@
-import type React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import GallerySeparator from '../Gallery/GallerySeparator/GallerySeparator';
-import { paths, labels, getNavLinkClasses } from '@/routes/navigationPaths';
+import type React from "react";
+import { Link, useLocation } from "react-router-dom";
+import GallerySeparator from "../Gallery/GallerySeparator/GallerySeparator";
+import { paths, labels, getNavLinkClasses } from "@/routes/navigationPaths";
 
 export const Sidebar: React.FC = () => {
   const location = useLocation();
 
   const isActive = (path: string): boolean => location.pathname === path;
-  const isMomentsActive = (): boolean => location.pathname.startsWith(paths.moments);
+  const isMomentsActive = (): boolean =>
+    location.pathname.startsWith(paths.moments);
 
   return (
     <div className="hidden md:block lg:block bg-stone-50">
@@ -23,89 +24,134 @@ export const Sidebar: React.FC = () => {
 
           <div className="flex">
             <Link to={paths.home} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.home))}>{labels.home}</span>
+              <span className={getNavLinkClasses(isActive(paths.home))}>
+                {labels.home}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.gruyeres} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isMomentsActive())}>{labels.moments}</span>
+              <span className={getNavLinkClasses(isMomentsActive())}>
+                {labels.moments}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.gruyeres} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.gruyeres), true)}>{labels.gruyeres}</span>
+              <span
+                className={getNavLinkClasses(isActive(paths.gruyeres), true)}
+              >
+                {labels.gruyeres}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.lausanne} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.lausanne), true)}>{labels.lausanne}</span>
+              <span
+                className={getNavLinkClasses(isActive(paths.lausanne), true)}
+              >
+                {labels.lausanne}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.lavaux} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.lavaux), true)}>{labels.lavaux}</span>
+              <span className={getNavLinkClasses(isActive(paths.lavaux), true)}>
+                {labels.lavaux}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.zurich} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.zurich), true)}>{labels.zurich}</span>
+              <span className={getNavLinkClasses(isActive(paths.zurich), true)}>
+                {labels.zurich}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.zurichHome} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.zurichHome), true)}>{labels.zurichHome}</span>
+              <span
+                className={getNavLinkClasses(isActive(paths.zurichHome), true)}
+              >
+                {labels.zurichHome}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.arosa} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.arosa), true)}>{labels.arosa}</span>
+              <span className={getNavLinkClasses(isActive(paths.arosa), true)}>
+                {labels.arosa}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.vidy} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.vidy), true)}>{labels.vidy}</span>
+              <span className={getNavLinkClasses(isActive(paths.vidy), true)}>
+                {labels.vidy}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.basel} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.basel), true)}>{labels.basel}</span>
+              <span className={getNavLinkClasses(isActive(paths.basel), true)}>
+                {labels.basel}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.autumn_walk} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.autumn_walk), true)}>{labels.autumn_walk}</span>
+              <span
+                className={getNavLinkClasses(isActive(paths.autumn_walk), true)}
+              >
+                {labels.autumn_walk}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.piedmont} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.piedmont), true)}>{labels.piedmont}</span>
+              <span
+                className={getNavLinkClasses(isActive(paths.piedmont), true)}
+              >
+                {labels.piedmont}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.autumn_garden} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.autumn_garden), true)}>{labels.autumn_garden}</span>
+              <span
+                className={getNavLinkClasses(
+                  isActive(paths.autumn_garden),
+                  true
+                )}
+              >
+                {labels.autumn_garden}
+              </span>
             </Link>
           </div>
 
           <div className="flex">
             <Link to={paths.sean} className="flex-1 px-4 pt-2">
-              <span className={getNavLinkClasses(isActive(paths.sean), true)}>{labels.sean}</span>
+              <span className={getNavLinkClasses(isActive(paths.sean), true)}>
+                {labels.sean}
+              </span>
             </Link>
           </div>
         </div>
-        <div className="flex-0 text-sm p-4 text-slate-500">© Chloe Ryder Photography</div>
+        <div className="flex-0 text-sm p-4 text-slate-500">
+          © Chloe Ryder Photography!
+        </div>
       </div>
     </div>
   );
